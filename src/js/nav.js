2e0d7e7ba28f4e4dd0d14c3bb6e0c5ec4fadc52f
ansaryergesh <yergeshovansar@gmail.com>
2020-01-26 06:42:17 +0600

    fixing eslint

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,5 @@
 const nav = () => {
   const nav = document.createElement('nav');
-  const navLists = () => {
-    return ();
-  };
   nav.innerHTML = `<div class="nav-wrapper">
   <a href="#" class="brand-logo">Lounge Bar</a>
   <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
