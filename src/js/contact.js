0e6db857488dfe9e249ad112e1e09a1d3948d870
ansaryergesh <yergeshovansar@gmail.com>
2020-01-26 06:37:34 +0600

    fixing eslint

--- src/js/contact.js
+++ src/js/contact.js
@@ -7,31 +7,27 @@ const contact = (() => {
   const form = document.createElement('form');
   form.classList.add('form');
   row.appendChild(form);
-
-  const formItems = () => {
-    return (`<div class="row">
-      <div class="input-field col s6">
-        <i class="material-icons prefix">account_circle</i>
-        <input id="icon_prefix" type="text" class="validate">
-        <label for="icon_prefix">First Name</label>
-      </div>
-      <div class="input-field col s6">
-        <i class="material-icons prefix">mode_edit</i>
-        <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-        <label for="icon_prefix2">Message</label>
-      </div>
-      <button class="btn waves-effect waves-light center" type="submit" name="action">Submit
-          <i class="material-icons right">send</i>
-      </button>
-      </div>`);
-  };
   const render = () => {
     header.textContent = 'Contact us';
     text.classList.add('none');
     other.classList.add('none');
     map.classList.add('none');
     form.classList.remove('none');
-    form.innerHTML = formItems();
+    form.innerHTML = `<div class="row">
+    <div class="input-field col s6">
+      <i class="material-icons prefix">account_circle</i>
+      <input id="icon_prefix" type="text" class="validate">
+      <label for="icon_prefix">First Name</label>
+    </div>
+    <div class="input-field col s6">
+      <i class="material-icons prefix">mode_edit</i>
+      <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+      <label for="icon_prefix2">Message</label>
+    </div>
+    <button class="btn waves-effect waves-light center" type="submit" name="action">Submit
+        <i class="material-icons right">send</i>
+    </button>
+    </div>`;
     if (document.querySelector('.card') !== null) {
       document.querySelector('.card').classList.add('none');
     }
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,18 +1,18 @@
 const nav = () => {
   const nav = document.createElement('nav');
   const navLists = () => {
-    return (`<div class="nav-wrapper">
-      <a href="#" class="brand-logo">Lounge Bar</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a class="home">Home</a></li>
-        <li><a class="menu">Menu</a></li>
-        <li><a class="about">About</a></li>
-        <li><a class="contact">Contact us</a></li>
-      </ul>
-    </div>`);
+    return ();
   };
-  nav.innerHTML = navLists();
+  nav.innerHTML = `<div class="nav-wrapper">
+  <a href="#" class="brand-logo">Lounge Bar</a>
+  <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+  <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <li><a class="home">Home</a></li>
+    <li><a class="menu">Menu</a></li>
+    <li><a class="about">About</a></li>
+    <li><a class="contact">Contact us</a></li>
+  </ul>
+  </div>`;
   return nav;
 };
 
