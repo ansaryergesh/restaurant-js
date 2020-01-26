08906190c0b6b4abe89dc18081fd83867c9335d8
ansaryergesh <yergeshovansar@gmail.com>
2020-01-26 06:39:41 +0600

    fixing eslint

--- src/js/home.js
+++ src/js/home.js
@@ -5,11 +5,7 @@ const home = (() => {
   const other = document.querySelector('.other');
   section.classList.add('section');
   const map = document.querySelector('.map');
-  const image = () => {
-    return `<img class="photo" src="https://i9.photo.2gis.com/images/branch/67/9429411758101449_fc9b.jpg">`    
-  };
-  other.innerHTML = image();
-
+  other.innerHTML = `<img class="photo" src="https://i9.photo.2gis.com/images/branch/67/9429411758101449_fc9b.jpg">`;
   const render = () => {
     header.textContent = 'Welcome to the Lounge Bar';
     text.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia numquam voluptates provident eius, illum totam pariatur voluptatem sit iste nulla quod labore tempore eos rem officia ad ex corrupti dolores!';
